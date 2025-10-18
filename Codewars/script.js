e250e95f4fa21c41bc16500eb52750d633026e07
pavelbeketov <mgbeketovpo@gmail.com>
2025-10-19 00:38:53 +0300

    refactor: update comments and clean up unused functions in script.js

--- Codewars/script.js
+++ Codewars/script.js
@@ -17,10 +17,34 @@ function clearLogs() {
 //==================================================================
 
 
+//==================================================================// day 1 number to string
+// function numberToString(num) {
+//   console.log(num.toString());
+// }
 
-function numberToString(num) {
-  console.log(num.toString());
-}
-
- //numberToString(333123324);
+//  numberToString(333123324);
  
+//==================================================================//day 1 sum arr                                   
+//  function sum (numbers) {
+//   let result = 0;
+//   let i = 0;
+//   while (i <= numbers.length - 1) {
+//       result = result + numbers[i];
+//       i = i + 1;// i++
+
+//   }
+
+
+//   return result;
+// }
+
+// sum([]);
+
+//or
+
+// function sum(numbers) {
+//   return numbers.reduce((a, b) => a + b, 0);
+// }
+
+
+// sum([1, 5, 5, 3, 3]);
